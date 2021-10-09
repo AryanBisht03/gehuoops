e6bce42ba549259a1ef5498a6d2c9f3fb5da990b
AryanBisht03 <92202194+AryanBisht03@users.noreply.github.com>
2021-10-09 12:29:17 +0530

    Add files via upload

---
+++ aryan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class base
+{
+public:
+int x;
+cin>>x;
+
+//int y;
+//cout<<"enter the value of y"<<endl;
+//cin>>y;
+//cout<<y;
+};
+int main()
+{
+base b1;
+cout<<b1.x<<endl;
+//cout<<b1.y<<endl;
+return 0;
+}
